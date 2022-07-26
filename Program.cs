878726026a7614759d0db8a05a035ed0fafd2a9b
Staxxx <sersersin@gmail.com>
2022-07-26 23:31:30 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//Написать программу, которая на вход принимает два числа
+//и выдает какое число больше
+
+//Пользователь вводит 2 числа a и b
+Console.Write("Введите a ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите b ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+//сравниваем 2 часла
+    if (a > b)
+    {
+    Console.WriteLine($"Больше a");
+    }
+
+    else
+    {
+    Console.WriteLine($"Больше b ");
+    }
+
